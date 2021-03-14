1ea99af1130e3fb36ed49e8b20068593eb68b963
batnikovN <49591835+Kusannnt@users.noreply.github.com>
2021-03-14 20:26:24 +0600

    Тусур 106 вариант 12 задача
    Тусур 106 вариант 10 задача

--- project/cpp/release/r1.cpp
+++ project/cpp/release/r1.cpp
@@ -10,25 +10,19 @@ int main()
 {
     int count, a;
     cin >> count;
-    int amin1 = INT_MAX, amin2 = INT_MAX, amax = 0;
+    int amin1 = INT16_MAX, amin2 = INT16_MAX, amax = 0;
     for (int i = 1; i <= count; i++)
     {
         cin >> a;
         if (a < amin1 || a < amin2)
-        {
             amin2 = amin1; amin1 = a;
-        }
         if (a > amax) amax = a;
     }
  
     if (amin1 < 0)
-    {
         cout << amin1 * amax << endl;
-    }
     if (amin1 > 0)
-    {
         cout << amin1 * amin2 << endl;
-    }
  
 }
  
