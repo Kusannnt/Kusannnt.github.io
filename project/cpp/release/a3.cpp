cc4c18173930acef9541e83d862a2960d075db0b
batnikovN <49591835+Kusannnt@users.noreply.github.com>
2021-03-15 00:18:59 +0600

    Create a3.cpp
    Тусур 102 вариант 10 задача

---
+++ project/cpp/release/a3.cpp
@@ -0,0 +1,22 @@
+
+/*
+___________________________
+
+Тусур 102 вариант 10 задача
+Является ли число автоморфным
+___________________________
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	setlocale(0, "");
+	int num, length, lenpow;
+	num; cin >> num;
+	length = (num == 0 ? 1 : int(log10(num) + 1)); 
+	lenpow = pow(10, length);
+	int term = (num*num) % lenpow;
+	if (num == term)cout << "Число автоаморфно" << "\n";
+	else cout << "Число не автоаморфно" << "\n";
+}
