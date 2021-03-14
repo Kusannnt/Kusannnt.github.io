38dbfba81d2c5a10a1338b96ce23d4849df931a6
batnikovN <49591835+Kusannnt@users.noreply.github.com>
2021-03-14 20:13:55 +0600

    Update r1.cpp

--- project/cpp/release/r1.cpp
+++ project/cpp/release/r1.cpp
@@ -1,1 +1,34 @@
+/*
+Тусур 106 вариант 12 задача
 
+Вывести минимальное произведение последовательности двух элементов
+*/
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int count, a;
+    cin >> count;
+    int amin1 = INT_MAX, amin2 = INT_MAX, amax = 0;
+    for (int i = 1; i <= count; i++)
+    {
+        cin >> a;
+        if (a < amin1 || a < amin2)
+        {
+            amin2 = amin1; amin1 = a;
+        }
+        if (a > amax) amax = a;
+    }
+ 
+    if (amin1 < 0)
+    {
+        cout << amin1 * amax << endl;
+    }
+    if (amin1 > 0)
+    {
+        cout << amin1 * amin2 << endl;
+    }
+ 
+}
+ 
