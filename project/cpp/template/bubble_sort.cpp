9adabbdc17ab6bf7903bd19f902716f1b42d5253
batnikovN <49591835+Kusannnt@users.noreply.github.com>
2021-03-14 22:05:00 +0600

    Create bubble_sort.cpp

---
+++ project/cpp/template/bubble_sort.cpp
@@ -0,0 +1,21 @@
+// Сортировка пузькомром по возрастанию
+for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+// Сортировка пузькомром по убываю
+for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
